a09f513f9152d8782d6dfbb666592670b8a98d85
Richard Pacheco <richardsp.dev@gmail.com>
2021-07-11 15:17:28 -0300

    fix(web): remove unknown props and attributes from Select component

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -15,9 +15,9 @@ const Select: React.FC<SelectProps> = (props) => {
   return (
     <div className="select-block">
       <label htmlFor={props.name}>{props.label}</label>
-      <select id={props.name} {...props}>
-        <option value="0" hidden disabled>
-          Selecione uma opção
+      <select id={props.name} name={props.name} defaultValue="">
+        <option value="" hidden disabled>
+          Selecione
         </option>
         {props.options.map(option => (
           <option key={option.value} value={option.value}>{option.label}</option>
